f0b1fa045d750d4ed7529cb96000bf455795fc26
LYuan22 <lucasyuan1528@gmail.com>
2021-07-12 16:40:07 -0400

    added image size stuff

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ import os
 import numpy as np
 
 
-def find_duplicates(self):
+def find_duplicates(dirname):
     fnames = os.listdir(dirname)
-    hashes = {}
+    image_dict = {}
     duplicates = []
     print("Finding Duplicates Now!\n")
     for image in fnames:
-        with Image.open(os.path.join(dirname, image)) as img:
-            temp_hash = imagehash.average_hash(img, 8)  # hashsize 8
-            if temp_hash in hashes:
-                print("Duplicate {} \nfound for Image {}!\n".format(
-                    image, hashes[temp_hash]))
-                duplicates.append(image)
+        path = os.path.join(dirname, image)
+        with Image.open(path) as img:
+            temp_hash = imagehash.average_hash(img, 8)  #hashsize 8
+            if temp_hash in image_dict:
+                #makes sure that the Original image is the one with higher resolution
+                if get_image_size(dirname, image, image_dict[temp_hash]):
+                    print("Duplicate: {} \nOriginal: {}!\n".format(
+                        image, image_dict[temp_hash]))
+                    duplicates.append(image)
+                else:
+                    print("Duplicate: {} \nOriginal: {}!\n".format(
+                        image_dict[temp_hash], image))
+                    duplicates.append(image_dict[temp_hash])
+                    image_dict[temp_hash] = image
             else:
-                hashes[temp_hash] = image
+                image_dict[temp_hash] = image
 
     if len(duplicates) != 0:
         return duplicates
@@ -25,7 +33,14 @@ def find_duplicates(self):
         print("No Duplicates Found :(")
         return None
 
-            
+#function gets the size of both images, returns True if image2 is larger
+def get_image_size(dir, image1, image2):
+    size1 = os.stat(os.path.join(dir, image1)).st_size
+    size2 = os.stat(os.path.join(dir, image2)).st_size
+    if size1 < size2:
+        return True
+    else:
+        return False
         
 """
 a = input("Do you want to delete these {} Images? Press Y or N:  ".format(len(duplicates)))
@@ -45,3 +60,4 @@ else:
 
 dirname = "Pictures"
 duplicates = find_duplicates(dirname)
+print(duplicates)
