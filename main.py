76e1d6938ed14a9f1b836482a16ac199bf1a8ce0
LYuan22 <lucasyuan1528@gmail.com>
2021-07-13 13:38:57 -0400

    small edits

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import os
 import numpy as np
 
 
-def find_duplicates(dirname):
-    fnames = os.listdir(dirname)
+def find_duplicates(directory):
+    fnames = os.listdir(directory)
     image_dict = {}
     duplicates = []
     print("Finding Duplicates Now!\n")
     for image in fnames:
-        path = os.path.join(dirname, image)
+        path = os.path.join(directory, image)
         with Image.open(path) as img:
             temp_hash = imagehash.average_hash(img, 8)  #hashsize 8
             if temp_hash in image_dict:
                 #makes sure that the Original image is the one with higher resolution
-                if get_image_size(dirname, image, image_dict[temp_hash]):
+                if get_image_size(directory, image, image_dict[temp_hash]):
                     print("Duplicate: {} \nOriginal: {}!\n".format(
                         image, image_dict[temp_hash]))
                     duplicates.append(image)
@@ -30,7 +30,7 @@ def find_duplicates(dirname):
     if len(duplicates) != 0:
         return duplicates
     else:
-        print("No Duplicates Found :(")
+        print("No Duplicates Found")
         return None
 
 #function gets the size of both images, returns True if image2 is larger
@@ -42,13 +42,13 @@ def get_image_size(dir, image1, image2):
     else:
         return False
         
-"""
+""" DELETE STUFF (RIPPED FORM OLD THING)
 a = input("Do you want to delete these {} Images? Press Y or N:  ".format(len(duplicates)))
 space_saved = 0
 if(a.strip().lower() == "y"):
     for duplicate in duplicates:
-        space_saved += os.path.getsize(os.path.join(self.dirname,duplicate))        
-        os.remove(os.path.join(self.dirname,duplicate))
+        space_saved += os.path.getsize(os.path.join(self.directory,duplicate))        
+        os.remove(os.path.join(self.directory,duplicate))
         print("{} Deleted Succesfully!".format(duplicate))
     
     print("\n\nYou saved {} mb of Space!".format(round(space_saved/1000000),2))
@@ -58,6 +58,6 @@ else:
 
 # Remove Duplicates
 
-dirname = "Pictures"
-duplicates = find_duplicates(dirname)
+directory = "Pictures"
+duplicates = find_duplicates(directory)
 print(duplicates)
