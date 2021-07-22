a1faea9ada1a4f0e7fa713e43f70b6c26fb0f085
LYuan22 <lucasyuan1528@gmail.com>
2021-07-22 14:05:04 -0400

    added size, similarity, and show image

--- GUI.py
+++ GUI.py
@@ -3,19 +3,13 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import sys
-from main import find_duplicates, similarity_to_hashsize, delete_picture
+from image_funcs import find_duplicates, similarity_to_hashsize, delete_picture, show_image, get_size, convert_size, get_similarity
 
-""" taskbar icon
-    Get rid of grid layout if possible
-    Change Hash Function
-    Enable Folder Drag and Drop
-    show in folder, open image buttons for pictures
-    Show stats (path, file size, similarity)
-"""
 
-SIMILARITY_LEVEL = 90
 FOLDERPATH = ''
 SPACE_SAVED = 0
+HASH_SIZE = 27
+SIMILARITY_LEVEL = 90
 
 
 class Ui_MainWindow(object):
@@ -64,7 +58,7 @@ class Ui_MainWindow(object):
 
 
 
-        #Sliders
+        #Slider
         self.Slider_Label = QLabel(self.centralwidget)
         self.Slider_Label.setGeometry(QRect(25, 73, 80, 22))
         self.Slider_Label.setObjectName("label")
@@ -134,7 +128,7 @@ class Ui_MainWindow(object):
         msg.setWindowTitle("Dupefinder")
         msg.setText("No Folder Selected")
         msg.setIcon(QMessageBox.Warning)
-        msg.setWindowIcon(QtGui.QIcon('logo.png'))
+        msg.setWindowIcon(QIcon('logo.png'))
         x = msg.exec()
 
     def slider_change(self):
@@ -142,6 +136,8 @@ class Ui_MainWindow(object):
         global SIMILARITY_LEVEL
         SIMILARITY_LEVEL = new_value
         self.similarity_text.setText(str(new_value)+ '%')
+        #global HASH_SIZE
+        #HASH_SIZE = similarity_to_hashsize(new_value)
 
     def get_dupes(self):
         if FOLDERPATH != '':
@@ -155,19 +151,25 @@ class Ui_MainWindow(object):
         #folder, Double click to open (or right click), size
         counter = 0
         orig_keys = originals.keys()
+        self.create_labels()
         for o_key in orig_keys:
             if o_key in duplicates:
                 orig = originals[o_key]
                 self.checkbox = QCheckBox(orig)
                 self.checkbox.setCheckState(Qt.Unchecked)
-                self.checkbox.setStyleSheet("background-color: rgb(120, 120, 120);")
-                self.scroll_GridLayout.addWidget(self.checkbox, counter, 0, 1,1)
+                self.checkbox.setStyleSheet("background-color: rgb(150, 150, 150);")
+                self.scroll_GridLayout.addWidget(self.checkbox, counter, 0, 1,2)
+                self.create_show_image_button(counter, orig)
+                self.create_size_text(counter, orig)
                 counter += 1
                 dupes = duplicates[o_key]
                 for dupe in dupes:
                     self.checkbox = QCheckBox(dupe)
                     self.checkbox.setCheckState(Qt.Unchecked)
-                    self.scroll_GridLayout.addWidget(self.checkbox, counter, 0, 1,1)
+                    self.scroll_GridLayout.addWidget(self.checkbox, counter, 0, 1,2)
+                    self.create_show_image_button(counter, dupe)
+                    self.create_size_text(counter, dupe)
+                    self.create_imgsimilarity_text(counter, orig, dupe)
                     counter += 1
         self.create_del_button()
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
@@ -185,21 +187,21 @@ class Ui_MainWindow(object):
     def delete_selected(self):
         selected = []
         counter = 0
+        space_saved = 0
         item = self.scroll_GridLayout.itemAt(counter)
         while item != None:
             widget = item.widget()
             if widget.isChecked():
-                global SPACE_SAVED
-                SPACE_SAVED += delete_picture(FOLDERPATH, widget.text())
+                space_saved += delete_picture(FOLDERPATH, widget.text())
                 selected.append(widget)
             counter = counter + 1
             item = self.scroll_GridLayout.itemAt(counter)
         self.get_dupes()
         #make a popup window saying how much space is saved, and a pop
-        if SPACE_SAVED == 0:
+        if space_saved == 0:
             self.none_selected_popup()
         else:
-            self.space_saved_popup()
+            self.space_saved_popup(space_saved)
         SPACE_SAVED = 0
 
     def none_selected_popup(self):
@@ -207,26 +209,69 @@ class Ui_MainWindow(object):
         msg.setWindowTitle("Dupefinder")
         msg.setText("Nothing Selected")
         msg.setIcon(QMessageBox.Warning)
-        msg.setWindowIcon(QtGui.QIcon('logo.png'))
+        msg.setWindowIcon(QIcon('logo.png'))
         x = msg.exec()
 
-    def space_saved_popup(self):
+    def space_saved_popup(self, space_saved):
         msg = QMessageBox()
         msg.setWindowTitle("Dupefinder")
-        msg.setText("Space Saved: " + str(SPACE_SAVED))
+        msg.setText("Space Saved: " + str(convert_size(space_saved)))
         msg.setIcon(QMessageBox.Information)
         msg.setWindowIcon(QIcon('logo.png'))
         x = msg.exec()
 
     def create_del_button(self): #2 Buttons to delete pictures
         self.Del_Dupes_Button = QPushButton(self.centralwidget)
-        self.Del_Dupes_Button.setGeometry(QRect(20, 130, 100, 23))
+        self.Del_Dupes_Button.setGeometry(QRect(130, 100, 100, 23))
+        self.Del_Dupes_Button.setStyleSheet("background-color: rgb(220, 220, 220);")
         self.Del_Dupes_Button.setObjectName("Del_Dupes_Button")
         self.Del_Dupes_Button.setText("Delete Selected")
-        self.Del_Dupes_Button.setStyleSheet("background-color: rgb(220, 220, 220);")
         self.Del_Dupes_Button.clicked.connect(self.delete_selected)
         self.Del_Dupes_Button.show()
-        
+
+
+
+    def create_show_image_button(self, counter, name):
+        self.show_image_button = QPushButton()
+        self.show_image_button.setStyleSheet("background-color: rgb(220, 220, 220);")
+        self.show_image_button.setObjectName("show_image_button")
+        self.show_image_button.setText("Show Image")
+        self.scroll_GridLayout.addWidget(self.show_image_button, counter, 4, 1,1)
+        self.show_image_button.clicked.connect(lambda: show_image(FOLDERPATH, name))
+
+    
+
+    def create_size_text(self, counter, name):
+        self.size_text = QLabel()
+        self.size_text.setObjectName("size_Text")
+        self.size_text.setText(get_size(FOLDERPATH, name))
+        self.scroll_GridLayout.addWidget(self.size_text, counter, 2, 1,1)
+
+    def create_imgsimilarity_text(self, counter, orig, dupe):
+        self.imgsimilarity_text = QLabel()
+        self.imgsimilarity_text.setObjectName("similarity_text")
+        self.imgsimilarity_text.setText(get_similarity(FOLDERPATH, orig, dupe))
+        self.scroll_GridLayout.addWidget(self.imgsimilarity_text, counter, 3, 1, 1)
+
+    def create_labels(self):
+        self.name_label = QLabel(self.centralwidget)
+        self.name_label.setObjectName("name_label")
+        self.name_label.setText("Name")
+        self.name_label.setGeometry(QRect(75, 145, 100, 23))
+        self.name_label.show()
+
+        self.size_label = QLabel(self.centralwidget)
+        self.size_label.setObjectName("size_label")
+        self.size_label.setText("Size")
+        self.size_label.setGeometry(QRect(260, 145, 100, 23))
+        self.size_label.show()
+
+        self.similarity_label = QLabel(self.centralwidget)
+        self.similarity_label.setObjectName("similarity_label")
+        self.similarity_label.setText("Similarity")
+        self.similarity_label.setGeometry(QRect(370, 145, 100, 23))
+        self.similarity_label.show()
+
 
 if __name__ == "__main__":
     import sys
