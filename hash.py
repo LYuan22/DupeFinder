9c35d2fde710845652b62b0c401d377696d3544f
LYuan22 <lucasyuan1528@gmail.com>
2021-07-23 16:04:24 -0400

    removed need for class

--- hash.py
+++ hash.py
@@ -3,46 +3,10 @@ import numpy
 
 
 def _binary_array_to_hex(arr):
-	#internal function to make a hex string out of a binary array.
+	#Converts Binary array into hex
 	bit_string = ''.join(str(b) for b in 1 * arr.flatten())
 	width = int(numpy.ceil(len(bit_string)/4))
 	return '{:0>{width}x}'.format(int(bit_string, 2), width=width)
-
-
-class ImageHash(object):
-	def __init__(self, binary_array):
-		self.hash = binary_array
-
-	def __str__(self):
-		return _binary_array_to_hex(self.hash.flatten())
-
-	def __repr__(self):
-		return repr(self.hash)
-
-	def __sub__(self, other):
-		if other is None:
-			raise TypeError('Other hash must not be None.')
-		if self.hash.size != other.hash.size:
-			raise TypeError('ImageHashes must be of the same shape.', self.hash.shape, other.hash.shape)
-		return numpy.count_nonzero(self.hash.flatten() != other.hash.flatten())
-
-	def __eq__(self, other):
-		if other is None:
-			return False
-		return numpy.array_equal(self.hash.flatten(), other.hash.flatten())
-
-	def __ne__(self, other):
-		if other is None:
-			return False
-		return not numpy.array_equal(self.hash.flatten(), other.hash.flatten())
-
-	def __hash__(self):
-		# this returns a 8 bit integer, intentionally shortening the information
-		return sum([2**(i % 8) for i, v in enumerate(self.hash.flatten()) if v])
-
-	def __len__(self):
-		# Returns the bit length of the hash
-		return self.hash.size
 
 
 def average_hash(image, hash_size = 8, mean=numpy.mean):
@@ -50,13 +14,23 @@ def average_hash(image, hash_size = 8, mean=numpy.mean):
 		raise ValueError("Hash size must be greater than or equal to 2")
 	#shrinks image down to hash_size x hash_size
 	image = image.convert("L").resize((hash_size, hash_size), Image.ANTIALIAS)
-
 	# find average pixel value; 'pixels' is an array of the pixel values, ranging from 0 (black) to 255 (white)
 	pixels = numpy.asarray(image)
 	avg = mean(pixels)
-
 	#Creation of bitstring
-	diff = pixels > avg
-	
+	hash = pixels > avg
 	#Creates the hash
-	return ImageHash(diff)
+	return hash
+
+def similarity(hash1, hash2):
+	#checks number of differences between two arrays
+	similarity = numpy.count_nonzero(hash1.flatten() != hash2.flatten())
+	return similarity
+
+def hash_equals(hash1, hash2):
+	#checks if both arrays are equal
+	return numpy.array_equal(hash1.flatten(), hash2.flatten())
+
+def hash_to_hex(hash):
+	string = _binary_array_to_hex(hash)
+	return string
